865c4603af6041e1efb6f3c406afcf0019f8ba5c
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-07-28 16:49:40 +0300

    Add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: "web-service",
+      script: "web-service/web-service.js",
+      env_production: {
+        NODE_ENV: "production"
+      }
+    },
+    {
+      name: "processor-service",
+      script: "processor-service/processor-service.js",
+      env_production: {
+        NODE_ENV: "production"
+      }
+    },
+    {
+      name: "cache-service",
+      script: "cache-service/server.js",
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
