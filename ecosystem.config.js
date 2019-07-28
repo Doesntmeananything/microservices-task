5bc6ee3766aea1778fbadc087159cbe8f8eade52
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-07-28 17:19:17 +0300

    Add concurrent script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,24 +5,21 @@ module.exports = {
       script: "web-service/web-service.js",
       env_production: {
         NODE_ENV: "production"
-      },
-      exec_mode: "cluster"
+      }
     },
     {
       name: "processor-service",
       script: "processor-service/processor-service.js",
       env_production: {
         NODE_ENV: "production"
-      },
-      exec_mode: "cluster"
+      }
     },
     {
       name: "cache-service",
       script: "cache-service/server.js",
       env_production: {
         NODE_ENV: "production"
-      },
-      exec_mode: "cluster"
+      }
     }
   ]
 };
