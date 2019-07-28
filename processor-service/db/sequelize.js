2c599be354a65d10e0b8bdaa6af25118579e50ae
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-07-28 18:17:35 +0300

    Add better success log message

--- processor-service/db/sequelize.js
+++ processor-service/db/sequelize.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(process.env.DB_URL);
 sequelize
   .authenticate()
   .then(() => {
-    console.log("Connection has been established successfully.");
+    console.log("Database connection has been established successfully.");
   })
   .catch(err => {
     console.error("Unable to connect to the database:", err);
