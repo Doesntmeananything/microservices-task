c24ae4683801fcea179fbcd901bf1f30aea647fb
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-07-28 16:57:44 +0300

    Add cluster exec mode for all aps

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,21 +5,24 @@ module.exports = {
       script: "web-service/web-service.js",
       env_production: {
         NODE_ENV: "production"
-      }
+      },
+      exec_mode: "cluster"
     },
     {
       name: "processor-service",
       script: "processor-service/processor-service.js",
       env_production: {
         NODE_ENV: "production"
-      }
+      },
+      exec_mode: "cluster"
     },
     {
       name: "cache-service",
       script: "cache-service/server.js",
       env_production: {
         NODE_ENV: "production"
-      }
+      },
+      exec_mode: "cluster"
     }
   ]
 };
