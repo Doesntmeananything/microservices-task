5c9df93f0ee4440bf232fbfb1b3dd10237cdc3c1
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-07-28 15:26:11 +0300

    Add default port

--- cache-service/server.js
+++ cache-service/server.js
@@ -38,6 +38,6 @@ app.get("/cache/top5", (req, res) => {
   });
 });
 
-const PORT = process.env.CACHE_SERVICE_PORT;
+const PORT = process.env.CACHE_SERVICE_PORT || 3000;
 
 app.listen(PORT, () => console.log("Server listening on port: ", PORT));
